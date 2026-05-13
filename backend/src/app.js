1c7dce211f09ce53f98429998e4407a483a22a89
Km Abdullah <kmabdullah682@gmail.com>
2026-05-13 18:14:57 +0600

    auth route created

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js"
 
 const app = express();
+
+
+app.use(cookieParser());
+app.use(express.json());
+
+//! Routes
+app.use("/api/auth", authRouter);
+
 
 
 export { app };
