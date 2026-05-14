129d5bb70230abb92087334df3c5931cf7f2a7e9
Km Abdullah <kmabdullah682@gmail.com>
2026-05-15 01:49:53 +0600

    get applications done

--- backend/src/controllers/application.controller.js
+++ backend/src/controllers/application.controller.js
@@ -56,4 +56,45 @@ async function apply(req, res) {
 };
 
 
-export { apply }
+async function getAllApplications (req , res) {
+    
+    try {
+
+        const employerId = req.userId;
+
+        const createdJobPosts = await Job.find({ employer: employerId });
+
+        if (createdJobPosts.length === 0) {
+            return res.status(404).json({
+                message: "Theres no jobs create by you"
+            });
+        };
+
+        const jobIds = createdJobPosts.map((job) => job._id);
+
+        const applications = await Application.find({ jobId: { $in: jobIds }}).sort({ createdAt: -1 }).populate("seekerId" , "username email iamge").populate("jobId" , "title description");
+        
+
+        if (!applications) {
+            return res.status(404).json({
+                message: "No applications on this job yet"
+            });
+        };
+
+
+        return res.status(200).json({
+            message: "applications fetched successfully",
+            applications: applications
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({
+            message: "internal server error"
+        });
+    };
+
+};
+
+
+export { apply, getAllApplications };
--- backend/src/routes/application.routes.js
+++ backend/src/routes/application.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { isUser } from "../middlewares/auth.middleware.js";
-import { apply } from "../controllers/application.controller.js";
+import { isEmployer, isUser } from "../middlewares/auth.middleware.js";
+import { apply, getAllApplications } from "../controllers/application.controller.js";
 
 const router = express.Router();
 
 
 router.post("/apply/:jobId", isUser, apply);
+router.get("/getApplications", isEmployer, getAllApplications);
 
 
 export default router;
