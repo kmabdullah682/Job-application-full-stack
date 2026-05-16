f2ac3fa80701808b7dfc4ec2df01bc57aaaf38fc
Km Abdullah <kmabdullah682@gmail.com>
2026-05-17 01:31:41 +0600

    hero section finished (1)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <main className="min-h-screen w-full relative">  
-        <Navbar />
+      <Navbar />
+      <HeroSection />
     </main>
   )
 }
---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,24 @@
+
+
+const HeroSection = () => {
+  return (
+      <section className="flex items-center justify-center flex-col gap-7 bg-linear-to-b from-white to-[#EAF1FF] h-[130vh]">
+          <div className="bg-[#6FFBBE] w-80 tracking-wide rounded-full px-2 py-1 text-green-900 font-semibold uppercase text-center">Transform your carrer journey</div>
+          <div className="flex flex-col gap-4">
+                <h1 className="text-black font-bold capitalize text-5xl">Where ambition meets</h1>
+                <h2 className="text-transparent bg-linear-to-r from-[#00885D] to-[#009ADA] bg-clip-text font-bold capitalize text-5xl">Seamless opportunity.</h2>
+          </div>
+          <span className="text-center text-[#68788F]">The next-generation job portal for perfect applicants and high-growth companies.<br /> Clean, clear, and focused on your professional growth.
+          </span>
+          
+          <div className="bg-white shadow-2xl w-150 h-20 rounded-xl mt-10 px-6 py-2 flex items-center gap-5">
+              <input type="text" className="bg-white outline-none border-b border-gray-200" placeholder="Job Keyword , Title"/>
+              <input type="text" className="bg-white outline-none border-b border-gray-200" placeholder="Min salary" />
+              <button className="bg-[#00885D] h-3/4 w-full rounded-xl text-lg font-bold text-white cursor-pointer transition-all duration-500 hover:bg-[#006C49]">Search Jobs</button>
+         </div>
+
+    </section>
+  )
+}
+
+export default HeroSection
